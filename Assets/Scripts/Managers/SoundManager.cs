ffd1815cec33a9175317484e86dadc79f27d137a
antony999k <lv999k@hotmail.com>
2020-03-03 23:50:34 -0600

    :sparkles: SoundManager Added

--- Assets/Scripts/Managers/SoundManager.cs
+++ Assets/Scripts/Managers/SoundManager.cs
@@ -1,18 +1,99 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+
+[System.Serializable]
+public class Sound
+{
+    public string name;
+    public AudioClip clip;
+
+    [Range(0, 1)]
+    public float volume = 0.7f;
+    public float pitch = 1f;
+    public bool loop;
+
+    private AudioSource source;
+
+    public void SetSource(AudioSource _source)
+    {
+        source = _source;
+        source.clip = clip;
+    }
+
+    public void Play()
+    {
+        source.volume = volume;
+        source.loop = loop;
+        source.Play();
+    }
+
+    public void Stop()
+    {
+        source.Stop();
+    }
+}
 
 public class SoundManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // Start Singleton
+    public static SoundManager instance;
+
+    private void Awake()
     {
-        
+        MakeSingleton();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void MakeSingleton()
     {
-        
+        if (instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+    // End Singleton
+    [SerializeField] Sound[] sounds;
+
+    private void Start()
+    {
+        for (int i = 0; i < sounds.Length; i++)
+        {
+            GameObject _go = new GameObject("Sound" + sounds[i].name);
+            _go.transform.SetParent(transform);
+            sounds[i].SetSource(_go.AddComponent<AudioSource>());
+        }
+    }
+
+    public void PlaySound(string _name)
+    {
+        for (int i = 0; i < sounds.Length; i++)
+        {
+            if (sounds[i].name == _name)
+            {
+                sounds[i].Play();
+                return;
+            }
+        }
+
+        //No sound with name
+        Debug.LogError("SoundManager: No sound with name " + _name);
+    }
+
+    public void PauseSound(string _name)
+    {
+        for (int i = 0; i < sounds.Length; i++)
+        {
+            if (sounds[i].name == _name)
+            {
+                sounds[i].Stop();
+                return;
+            }
+        }
+
+        //No sound with name
+        Debug.LogError("SoundManager: No sound with name " + _name);
     }
 }
